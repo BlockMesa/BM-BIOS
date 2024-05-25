7a02d01f0cef16d97de631e33adfbc01105fb656
bronwyn <119465870+notbronwyn@users.noreply.github.com>
2024-05-25 14:31:20 -0400

    Add blank functions so Capy64 apps can control RPC without breaking compat

--- startup.lua
+++ startup.lua
@@ -1,6 +1,9 @@
 --Startup file for CC:Tweaked
 local compat = {}
 compat.isCapy64 = false
+compat.title = function(...) return end
+compat.setRPC = function(...) return end
+compat.log = function(...) return end
 _G.compat = compat
 
 os.run({},".BIOS")
